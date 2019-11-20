5482ede1aa15287c78a48c0657f4e1dcfdd6cabd
Mirjana Šuput <mirjanasuput4@gmail.com>
2019-11-20 15:49:54 +0100

    Adding input fields, alphabet array, color changing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState,useRef} from 'react';
+
 import './App.css';
 
 function App() {
+  const [alphabet,setAlphabet] = useState('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(el => ({
+    letter: el,
+    status: 'left'
+  })))
+  const [randomNumber,setRandomNumber] = useState(Math.floor(Math.random() * ( Math.floor(26)-Math.ceil(1))) + 1)
+  const [selectedDifficulty,setSelectedDifficulty] = useState(0)
+  const [inProgress,setInProgress] = useState(false)
+
+  const difficulties = [
+    {
+      id:0,
+      title:'Easy',
+      timeout:5000
+    },
+    {
+      id:1,
+      title:'Medium',
+      timeout:3500
+    },
+    {
+      id:2,
+      title:'Hard',
+      timeout:2000
+
+    }
+  ]
+
+  const input = useRef("")
+
+  const valid = () => {
+    if(input.current.value.toUpperCase() === alphabet[randomNumber-1].letter){
+      alphabet[randomNumber-1].status='correct'
+    }else{
+      alphabet[randomNumber-1].status='invalid'
+    }
+    setAlphabet([...alphabet])
+  }
+  let interval
+  const start =  () =>{
+    setInProgress(true)
+    interval = setInterval(() => {
+      setRandomNumber(Math.floor(Math.random() * ( Math.floor(26)-Math.ceil(1))) + 1)
+    }, difficulties[selectedDifficulty].timeout);
+    console.log(interval)
+    
+  }
+  const stop =  () =>{
+    setInProgress(false)
+    alphabet.forEach(i=>i.status='left')
+    setAlphabet([...alphabet])
+   console.log(interval)
+    interval = clearInterval(interval) 
+
+
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>           
+    <button onClick={inProgress ? stop : start}>{inProgress?'Stop':'Start'}</button>
+      <div className="radio-buttons-container">
+      {
+         difficulties.map((i,index) => 
+          <><input type="radio" name="difficulty" checked={selectedDifficulty === i.id} key={i.id} onChange={()=>setSelectedDifficulty(index)} disabled={inProgress}/> {i.title}</>
+        )
+      }
+      </div>
+      <input type="text" ref={input} onChange={valid} disabled={!inProgress}/>
+      {randomNumber}
+      <div id="alphabet-container">
+        {
+          alphabet.map(
+            (i,index) =>
+              <span key={index} className="letter" style={i.status==="left" ? {color: 'blue'} : i.status==='invalid' ? {color: 'red'}:{color:'green'}}>{i.letter}</span>
+            )
+        }
+      </div>
+        Hit:{alphabet.filter(i=>i.status==="correct").length}
+        Miss:{alphabet.filter(i=>i.status==="invalid").length}
+        Left:{alphabet.filter(i=>i.status==="left").length}
+    </>
   );
 }
 
